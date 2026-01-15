0d210e2d2a8889bb3b98222916d75a81ac4bdb18
Andalexskywalker <andalex.ramos04@gmail.com>
2026-01-15 14:35:37 +0000

    Final polish: Fixed frontend startup, added docs and updated gitignore

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-352e0b0a3e93d745.js","static/chunks/45bb6821-44d53b7890495b51.js","static/chunks/687-2c9ba65055d52bf4.js","static/chunks/main-app-8a88838ea6738688.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-352e0b0a3e93d745.js","static/chunks/framework-4b8e51f9c72ab23b.js","static/chunks/main-38a204fdd09005ac.js","static/chunks/pages/_app-a4df0293388517f2.js"],"/_error":["static/chunks/webpack-352e0b0a3e93d745.js","static/chunks/framework-4b8e51f9c72ab23b.js","static/chunks/main-38a204fdd09005ac.js","static/chunks/pages/_error-a429532b841d749d.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
